082bb58664abc622fdf3ecd17bcba4576ca2a4fb
lea.arturi <lea.arturi@gmail.com>
2022-01-15 12:53:35 -0300

    Instalacion @wordpress/scripts

---
+++ plugins/lapizzeria-gutenberg/lapizzeria-gutenberg.php
@@ -0,0 +1,17 @@
+<?php 
+    /* 
+        Plugin Name: La Pizzeria Gutenberg
+        Plugin URI:
+        Description: Plugin para agregar un bloque de contenido personalizado
+        Version: 1.0
+        Author: Leandro Arturi    
+        Author URI: https://leandroarturi.com.ar
+        License: GPL2
+        License URI: https://www.gnu.org/licenses/gpl-2.0.html   
+    */
+
+    if(!defined('ABSPATH')){
+        exit;
+    }
+
+?>
